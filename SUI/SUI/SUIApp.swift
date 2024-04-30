80050b185a60c9dba90fecad25366c45843c19d7
Anastasiya Omak <ios.developer.by@gmail.com>
2024-04-30 08:19:41 +0300

    Init commit

---
+++ SUI/SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by Anastasiya Omak on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
